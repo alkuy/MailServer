4cc1eceec751002f822411905edf9e6ca0b1d066
Diego Castelli <Diego Castelli@Diego>
2017-08-02 23:09:49 -0300

    Agregue las lineas para mejorar la interfaz grafica. se puede probar las que estan comentadas. Adrian va a arreglar un tema con la seleccion que da en usuarios y en cuenta. El mostrar dominio se ve bien.

--- MailServer/src/grafica/Ventanas.java
+++ MailServer/src/grafica/Ventanas.java
@@ -4,7 +4,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.Timer;
+import javax.swing.UIManager;
 
 import Logica.Fachada;
 import Persistencia.FachadaBD;
@@ -14,7 +17,20 @@ public class Ventanas {
 
 	public static void main(String[] args) throws InterruptedException, SQLException {
 		
-		
+		try{
+			  
+			  JFrame.setDefaultLookAndFeelDecorated(true);
+			  JDialog.setDefaultLookAndFeelDecorated(true);
+			  UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+			//UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+			 // UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"); // el que mas me gusto
+			 // UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+			 
+			}
+			catch (Exception e)
+			 {
+			  e.printStackTrace();
+			 }
 		// TODO Auto-generated method stub
 		Presentacion present = new Presentacion();
 		present.setLocationRelativeTo(null);  
