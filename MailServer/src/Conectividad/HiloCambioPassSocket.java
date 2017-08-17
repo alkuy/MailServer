860d9f4d2b6ae4cdc835b42e3b4739e5855b41ac
Diego Castelli <Diego Castelli@192.168.56.1>
2017-08-17 00:47:32 -0300

    Solucion cambio de clave desde cliente

--- MailServer/src/Conectividad/HiloCambioPassSocket.java
+++ MailServer/src/Conectividad/HiloCambioPassSocket.java
@@ -2,25 +2,6 @@ package Conectividad;
 
 import java.io.*;
 import java.net.*;
-import java.util.Properties;
-import java.sql.SQLException;
-
-import javax.mail.Folder;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Store;
-import javax.mail.URLName;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-import com.icegreen.greenmail.Managers;
-import com.icegreen.greenmail.store.FolderException;
-import com.icegreen.greenmail.user.GreenMailUser;
-import com.icegreen.greenmail.user.UserException;
-import com.icegreen.greenmail.util.GreenMail;
-import com.icegreen.greenmail.util.ServerSetupTest;
-
 import Logica.Fachada;
 
 
@@ -69,8 +50,9 @@ public class HiloCambioPassSocket extends Thread {
 
 			Signal = entrada.readUTF();
 			
+			
 			if (Signal.equals("OK")){
-				FC.cambiaPassCuenta(NomUsu, DomUsu, OldPass, Pass);
+				FC.cambiaPassCuenta(NomUsu, DomUsu, OldPass);
 			}else
 				System.out.println("Error en Conexion");
 		}catch (Exception e){}
--- MailServer/src/Logica/Fachada.java
+++ MailServer/src/Logica/Fachada.java
@@ -279,10 +279,11 @@ public void altaDominio(String dominio, int prioridad){
  	
  	
  	
-	public void cambiaPassCuenta(String nom_us, String nom_dom, String passviejo, String passNuevo){
- 		//System.out.println(id);
+ 	public void cambiaPassCuenta(String nom_us, String nom_dom,String passNuevo) throws SQLException{
+ 		
 		
-		int id = this.trae_id(passviejo);
+		int id = this.IdUsuario(nom_us, nom_dom);
+		System.out.println(id);
  		hUsu.find(id).getCuentas().cambiaPass(nom_us, nom_dom, passNuevo);
  	}
  	
